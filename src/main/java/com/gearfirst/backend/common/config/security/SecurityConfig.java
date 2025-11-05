57be2eb515fbd61ce1fdd9167ad97d9fa2dcaa46
qwice <134401970+qwice@users.noreply.github.com>
2025-11-05 11:07:37 +0900

    fix

--- src/main/java/com/gearfirst/backend/common/config/security/SecurityConfig.java
+++ src/main/java/com/gearfirst/backend/common/config/security/SecurityConfig.java
@@ -94,7 +94,7 @@ public class SecurityConfig {
                                                     HttpServletResponse response,
                                                     FilterChain filterChain)
                             throws ServletException, IOException {
-                        System.out.println("Request : " + request);
+                        System.out.println("Request : " + request.getHeader());
                         System.out.println(">>> Host: " + request.getHeader("Host"));
                         System.out.println(">>> X-Forwarded-Host: " + request.getHeader("X-Forwarded-Host"));
                         System.out.println(">>> X-Forwarded-Proto: " + request.getHeader("X-Forwarded-Proto"));
