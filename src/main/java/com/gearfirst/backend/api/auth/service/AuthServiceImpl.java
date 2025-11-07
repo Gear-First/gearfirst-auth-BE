831195b157868a9b40a639e84cfe1e6f814262b1
ksoheee <thgml0716030@naver.com>
2025-11-08 02:50:36 +0900

    feat: 임시 비밀번호 이메일로 발송하도록 변경

--- src/main/java/com/gearfirst/backend/api/auth/service/AuthServiceImpl.java
+++ src/main/java/com/gearfirst/backend/api/auth/service/AuthServiceImpl.java
@@ -40,8 +40,7 @@ public class AuthServiceImpl implements AuthService{
     public void createAccount(CreateAccount request) {
         String tempPassword = RandomStringUtils.random(10, 0, 0, true, true, null, new SecureRandom());
         String encodedPassword = passwordEncoder.encode(tempPassword);
-        log.info("tempPassword: {}", tempPassword);
-
+        //log.info("tempPassword: {}", tempPassword);
 
         // 이메일 중복 체크
         if(authRepository.findByEmail(request.getEmail()).isPresent()){
@@ -60,7 +59,7 @@ public class AuthServiceImpl implements AuthService{
                 @Override
                 public void afterCommit() {
                     try{
-                        //mailService.sendUserRegistrationMail(request.getPersonalEmail(), tempPassword);
+                        mailService.sendUserRegistrationMail(request.getPersonalEmail(), tempPassword);
                     }catch (Exception e) {
                         log.error("메일 발송 실패 - personalEmail={}, message={}", request.getPersonalEmail(), e.getMessage(), e);
                     }
@@ -68,7 +67,7 @@ public class AuthServiceImpl implements AuthService{
                 }
             });
         } else {
-            //mailService.sendUserRegistrationMail(request.getPersonalEmail(), tempPassword);
+            mailService.sendUserRegistrationMail(request.getPersonalEmail(), tempPassword);
         }
     }
 
@@ -100,7 +99,7 @@ public class AuthServiceImpl implements AuthService{
             @Override
             public void afterCommit() {
                 try {
-                    //mailService.sendUserRegistrationMail(personalEmail, newTempPassword);
+                    mailService.sendUserRegistrationMail(personalEmail, newTempPassword);
                 } catch (Exception e) {
                     throw new IllegalStateException("메일 발송 중 오류가 발생했습니다: " + e.getMessage());
                 }
